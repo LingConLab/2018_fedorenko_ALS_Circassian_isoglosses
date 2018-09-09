1abef646b58e1bc5c97d2bf3a3289723e45e2637
agricolamz <agricolamz@gmail.com>
2018-09-09 07:57:35 +0300

    add nice look and clusterisation

--- map creation example.R
+++ map creation example.R
@@ -17,10 +17,21 @@ df %>%
   left_join(circassian[,c(1:3, 6)], by = c("en_village" = "village")) ->
   df
 
+# look at the data
 df %>% 
   filter(feature == "бенефактивный послелог",
-         variable == "ой пае") -> t
-  map.feature(languages = t$language,
+         variable == "ой пае") %>% 
+  group_by(variable, grammaticality) %>% 
+  summarise(vilages = paste0(sort(en_village), collapse = ", "),
+            n = n())
+
+# visualise the data
+df %>% 
+  filter(feature == "бенефактивный послелог",
+         variable == "ой пае") ->
+  t
+
+map.feature(languages = t$language,
               features = t$grammaticality,
               latitude = t$latitude,
               longitude = t$longitude,
@@ -33,3 +44,41 @@ df %>%
               minimap = TRUE)
 
 
+# clusterisation
+# cluster villages
+df %>%
+  distinct() %>% 
+  mutate(grammaticality = if_else(grammaticality == "+", 1, 0)) %>% 
+  select(grammaticality, en_village, variable) %>% 
+  spread(variable, grammaticality) %>% 
+  as.data.frame() ->
+  matrix_v
+
+rownames(matrix_v) <- matrix_v$en_village
+matrix_v <- matrix_v[,-1]
+
+matrix_v %>% 
+  dist() %>% 
+  hclust() ->
+  hc
+
+plot(hc)
+
+# cluster features
+df %>%
+  distinct() %>% 
+  mutate(grammaticality = if_else(grammaticality == "+", 1, 0)) %>% 
+  select(grammaticality, en_village, variable) %>% 
+  spread(en_village, grammaticality) %>% 
+  as.data.frame() ->
+  matrix_f
+
+rownames(matrix_f) <- matrix_f$variable
+matrix_f <- matrix_f[,-1]
+
+matrix_f %>% 
+  dist() %>% 
+  hclust() ->
+  hc
+
+# it doesn't work because of a big amount of NA, but probably it will works, if you select some features
